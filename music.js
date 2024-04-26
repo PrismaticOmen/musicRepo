dc4e8dfadb4c25466d68e5ef35932074516bbad9
AllyssaJohnston <168232004+AllyssaJohnston@users.noreply.github.com>
2024-04-26 16:52:17 -0700

    chart

--- music.js
+++ music.js
@@ -1,13 +1,83 @@
 window.onload = init;
+let sliders;
+let printedValues;
+let xValues = [];
+let yValues = [];
+let musicGraph;
 
 function init()
 {
-	var slider = document.getElementById("range");
-	var printedValue = document.getElementById("sliderValue");
-	printedValue.innerHTML = slider.value;
-	
-	slider.oninput = function()
+	sliders = document.getElementsByClassName("slider");
+	printedValues = document.getElementsByClassName("sliderValue");
+	console.log("init");
+	for (let i = 0; i < sliders.length; i++)
 	{
-		printedValue.innerHTML = this.value;
+		updateSlider(i);
 	}
+	updateData();
+	
+	musicGraph = new Chart("musicGraph", 
+	{
+		type: "line",
+		data: 
+		{
+			labels: xValues,
+			datasets: 
+			[{
+				fill: false,
+				backgroundColor: "rgba(0, 0, 0, 1.0)",
+				pointRadius: 1,
+				data: yValues
+			}]
+		},
+		options:
+		{
+			title: 
+			{
+				display: true,
+				text: "music"
+			},
+			responsive: true,
+			maintainAspectRatio: false,
+			scales: 
+			{
+				yAxes: 
+				[{
+					
+					ticks: 
+					{
+						stepSize: 1
+					}
+				}]
+			}
+		}
+	})
+
 }
+
+function updateSlider(x)
+{
+	var slider = sliders[x];
+	var printedValue = printedValues[x];
+	printedValue.innerHTML = slider.value;
+	updateData();
+}
+
+function updateData()
+{
+	var f = parseFloat(document.getElementById("frequency").value);
+	var s = parseFloat(document.getElementById("speed").value);
+	var a = parseFloat(document.getElementById("acceleration").value);
+	for (let i = 0; i < 100; i++)
+	{
+		xValues[i] = (i);
+		console.log(f + (s*i) + (a**i));
+		yValues[i] = f + parseFloat(s*i) + parseFloat(a**2);
+		
+	}
+	if (musicGraph != undefined)
+	{
+		musicGraph.update();
+	}
+};
+	
