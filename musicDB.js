b314f949443c4aec36f73ff0ab8337007ab49222
AllyssaJohnston <168232004+AllyssaJohnston@users.noreply.github.com>
2024-04-27 14:10:42 -0700

    started array

---
+++ musicDB.js
@@ -0,0 +1,10 @@
+window.onload = init;
+
+function init()
+{
+	let inputMap = {};
+	var userInput = document.getElementById("chordInput");
+	const userInputKeys = userInput.split(' ');
+	
+	inputMap.push("chord progression");
+}
